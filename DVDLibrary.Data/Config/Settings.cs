4463a1a272e8c451aea848c57d41f7c3da2905bc
Jim Shaw <jimxshaw@gmail.com>
2015-11-09 22:59:49 -0500

    added references and necessary nuget packages

---
+++ DVDLibrary.Data/Config/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace DVDLibrary.Data.Config
+{
+    public static class Settings
+    {
+        private static string _connectionString;
+
+        public static string ConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_connectionString))
+                {
+                    _connectionString = ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+                }
+
+                return _connectionString;
+            }
+        }
+    }
+}
