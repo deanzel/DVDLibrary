73026f31d6f769ffd0270cfa5d98f29b3a65d73f
Chary Gurney <chary.gurney@gmail.com>
2015-11-11 16:32:34 -0500

    added Home Controller with options of View All Dvds and added Index page and Search to Nav Bar but not functional

--- DVDLibrary.UI/Controllers/HomeController.cs
+++ DVDLibrary.UI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DVDLibrary.Models;
 
 namespace DVDLibrary.UI.Controllers
 {
@@ -12,6 +13,18 @@ namespace DVDLibrary.UI.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ViewAllDvds()
+        {
+            List<Movie> movies = new List<Movie>()
+            {
+                new Movie {MovieId = 1, MovieTitle = "Frozen", MpaaRating = "G", ReleaseDate = DateTime.Parse("06/15/2013"), Synopsis = "It's a girly movie", Duration = 120},
+                new Movie {MovieId = 2, MovieTitle= "DieHard", MpaaRating  = "R", ReleaseDate = DateTime.Parse("10/20/1989"), Synopsis = "Great Action Film", Duration = 180},
+                new Movie {MovieId = 3, MovieTitle = "Dumb and Dumber", MpaaRating = "PG-13", ReleaseDate = DateTime.Parse("05/10/1995"), Synopsis = "Funny movie", Duration = 200}
+            };
+
+            return View(movies);
         }
     }
 }
