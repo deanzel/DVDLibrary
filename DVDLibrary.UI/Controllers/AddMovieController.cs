549956ceee529751583cda0b0667f886c603ae5c
Chary Gurney <chary.gurney@gmail.com>
2015-11-12 15:36:48 -0500

    added add movie controller

---
+++ DVDLibrary.UI/Controllers/AddMovieController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DVDLibrary.UI.Controllers
+{
+    public class AddMovieController : Controller
+    {
+        // GET: AddMovie
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
