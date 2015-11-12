910ae80041d86a02b5919187a8fc4567e70c1523
Chary Gurney <chary.gurney@gmail.com>
2015-11-12 13:59:11 -0500

    problem with getting poster url

--- DVDLibrary.UI/Controllers/HomeController.cs
+++ DVDLibrary.UI/Controllers/HomeController.cs
@@ -61,36 +61,40 @@ namespace DVDLibrary.UI.Controllers
 
         public ActionResult SelectDvd(int id)
         {
-            List<Movie> movies = new List<Movie>()
-            {
-                new Movie
-                {
-                    MovieId = 1,
-                    MovieTitle = "Frozen",
-                    MpaaRating = "G",
-                    ReleaseDate = DateTime.Parse("06/15/2013"),
-                    Synopsis = "It's a girly movie",
-                    Duration = 120
-                },
-                new Movie
-                {
-                    MovieId = 2,
-                    MovieTitle = "DieHard",
-                    MpaaRating = "R",
-                    ReleaseDate = DateTime.Parse("10/20/1989"),
-                    Synopsis = "Great Action Film",
-                    Duration = 180
-                },
-                new Movie
-                {
-                    MovieId = 3,
-                    MovieTitle = "Dumb and Dumber",
-                    MpaaRating = "PG-13",
-                    ReleaseDate = DateTime.Parse("05/10/1995"),
-                    Synopsis = "Funny movie",
-                    Duration = 200
-                }
-            };
+            _oops = new DVDLibaryOperations();
+
+            var movies = _oops.ReturnMoviesList();
+
+            //List<Movie> movies = new List<Movie>()
+            //{
+            //    new Movie
+            //    {
+            //        MovieId = 1,
+            //        MovieTitle = "Frozen",
+            //        MpaaRating = "G",
+            //        ReleaseDate = DateTime.Parse("06/15/2013"),
+            //        Synopsis = "It's a girly movie",
+            //        Duration = 120
+            //    },
+            //    new Movie
+            //    {
+            //        MovieId = 2,
+            //        MovieTitle = "DieHard",
+            //        MpaaRating = "R",
+            //        ReleaseDate = DateTime.Parse("10/20/1989"),
+            //        Synopsis = "Great Action Film",
+            //        Duration = 180
+            //    },
+            //    new Movie
+            //    {
+            //        MovieId = 3,
+            //        MovieTitle = "Dumb and Dumber",
+            //        MpaaRating = "PG-13",
+            //        ReleaseDate = DateTime.Parse("05/10/1995"),
+            //        Synopsis = "Funny movie",
+            //        Duration = 200
+            //    }
+            //};
 
             var result = movies.Where(m => m.MovieId == id).First();
 
