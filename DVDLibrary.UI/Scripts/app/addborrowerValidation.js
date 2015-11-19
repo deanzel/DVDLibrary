12318027104b488f46cf4b9d9abd16cc797b7e1a
Dean Choi <deanchoi@gmail.com>
2015-11-19 14:56:38 -0500

    Fixed some errors and removed unobtrusive validation from Add Borrowers to get special email validation back

---
+++ DVDLibrary.UI/Scripts/app/addborrowerValidation.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+    $('#addborrowerform').validate({
+        rules: {
+            FirstName: {
+                required: true
+            },
+            LastName: {
+                required: true
+            },
+            IsOwner: {
+                required: true
+            },
+            Email: {
+                required: true,
+                email: true
+            },
+            Phone: {
+                required: true
+            },
+            Address: {
+                required: true
+            },
+            City: {
+                required: true
+            },
+            State: {
+                required: true
+            },
+            Zipcode: {
+                required: true
+            }
+        },
+
+        messages: {
+            FirstName: "Please enter your first name",
+            LastName: "Please enter your last name",
+            IsOwner: "Please enter if you are the owner or not",
+            Email: {
+                required: "Enter your email address",
+                email: "That's not a correct format for an email address"
+            },
+            Phone: "Please enter your phone number",
+            Address: "Please enter your street address",
+            City: "Please enter your city",
+            State: "Please enter your state",
+            Zipcode: "Please enter your zipcode"
+        }
+    });
+});
