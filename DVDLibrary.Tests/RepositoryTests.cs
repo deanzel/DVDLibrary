658f12693e0b4e90618f331b9678274ded438cd7
Dean Choi <deanchoi@gmail.com>
2015-12-01 20:44:18 -0500

    Added unit testing notes and outline

--- DVDLibrary.Tests/RepositoryTests.cs
+++ DVDLibrary.Tests/RepositoryTests.cs
@@ -14,12 +14,20 @@ namespace DVDLibrary.Tests
         public void Init()
         {
             //var oops = new DVDLibaryOperations();
+            //Include SQL script (dbsetup.sql) and run via ADO
+            //Script drops TestDVDLibrary database and then creates it, the tables, stored procedures
+            //and then fills the data (everything except for comments so far)
+
+            //No using statement (keep SQL connection open)
+
+            //cn.open (no using statement so the SQL connection only opens up once)
         }
 
         [TestFixtureTearDown]
         public void Dispose()
         {
-
+            //cn.close
+            //cn.dispose
         }
 
         [Test]
