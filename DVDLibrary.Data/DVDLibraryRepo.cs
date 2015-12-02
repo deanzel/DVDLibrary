85968ad80ead264f865353fdbc23909325eb8189
Dean Choi <deanchoi@gmail.com>
2015-12-02 07:11:32 -0500

    More unit tests added for Data Layer; up to AddNewBorrower

--- DVDLibrary.Data/DVDLibraryRepo.cs
+++ DVDLibrary.Data/DVDLibraryRepo.cs
@@ -39,95 +39,6 @@ namespace DVDLibrary.Data
 
     public class DVDLibraryRepo : IDVDLibraryRepo
     {
-        //private List<DVD> ListOfDVDs;
-        //private List<Models.Movie> ListOfMovies;
-
-        //public DVDLibraryRepo()
-        //{
-        //    ListOfDVDs = new List<DVD>();
-        //    ListOfMovies = new List<Models.Movie>();
-
-        //}
-
-        ////Initialize a mock list of DVDs and movies
-        //public void InitializeMockDataRepo()
-        //{
-        //    for (int i = 11; i < 20; i++)
-        //    {
-        //        DVD newDVD = new DVD();
-        //        newDVD.DVDId = i - 10;
-        //        newDVD.DVDType = "Blu-Ray";
-
-        //        newDVD.Movie = ReturnMovieInfoFromTMDB(i);
-        //        newDVD.Movie.MovieId = i - 10;
-
-        //        //**need to show status info:
-        //        //Status status = new Status();
-        //        //{
-        //        //    StatusId = i - 10;
-        //        //}
-
-        //        UserNote newUserNote = new UserNote()
-        //        {
-        //            UserNoteId = i - 10,
-        //            BorrowerId = 1,
-        //            BorrowerName = "Dean Choi",
-        //            MovieId = i - 10,
-        //            Note = "Greatest. Movie. EVAR!!!!!",
-        //            Owner = true
-        //        };
-        //        newDVD.Movie.UserNotes.Add(newUserNote);
-
-        //        UserNote newUserNote2 = new UserNote()
-        //        {
-        //            UserNoteId = (i - 10)*100,
-        //            BorrowerId = 2,
-        //            BorrowerName = "Chary Gurney",
-        //            MovieId = i - 10,
-        //            Note = "FAIL!!!!!",
-        //            Owner = false
-        //        };
-        //        newDVD.Movie.UserNotes.Add(newUserNote2);
-
-        //        UserRating newUserRating = new UserRating()
-        //        {
-        //            BorrowerId = 1,
-        //            BorrowerName = "Dean Choi",
-        //            MovieId = i - 10,
-        //            Rating = 9,
-        //            UserRatingId = i - 10,
-        //            Owner = true
-        //        };
-        //        newDVD.Movie.UserRatings.Add(newUserRating);
-
-        //        UserRating newUserRating2 = new UserRating()
-        //        {
-        //            BorrowerId = 2,
-        //            BorrowerName = "Charey Gurney",
-        //            MovieId = i - 10,
-        //            Rating = 2,
-        //            UserRatingId = (i - 10)*100,
-        //            Owner = false
-        //        };
-        //        newDVD.Movie.UserRatings.Add(newUserRating2);
-
-        //        ListOfDVDs.Add(newDVD);
-        //        ListOfMovies.Add(newDVD.Movie);
-        //    }
-        //}
-
-        ////Return DVDList (mock)
-        //public List<DVD> RetrieveDVDsList()
-        //{
-        //    return ListOfDVDs;
-        //}
-
-        ////Return MoviesList (mock)
-        //public List<Models.Movie> RetrieveMoviesList()
-        //{
-        //    return ListOfMovies;
-        //}
-
         //Build ListOfMovies list from SQL Database method (just with basic info)
 
         public List<Models.Movie> RetrieveMoviesListFromDB()
@@ -719,6 +630,116 @@ namespace DVDLibrary.Data
         }
 
 
+        //Retrieve TMDB info with a TMDBNum
+        public Models.Movie ReturnMovieInfoFromTMDB(int tmdbNum)
+        {
+            TMDbClient client = new TMDbClient("1fee8f2397ff73412985de2bb825f020");
+
+            Movie movie = client.GetMovie(tmdbNum,
+                MovieMethods.AlternativeTitles | MovieMethods.Credits | MovieMethods.Images | MovieMethods.Releases |
+                MovieMethods.Videos);
+
+            Models.Movie movieInfo = new Models.Movie();
+            movieInfo.MovieTitle = movie.Title;
+            movieInfo.MovieTMDBNum = movie.Id;
+            if (movie.Releases.Countries.FirstOrDefault(m => m.Iso_3166_1 == "US") != null)
+            {
+                movieInfo.MpaaRating = movie.Releases.Countries.FirstOrDefault(m => m.Iso_3166_1 == "US").Certification;
+            }
+            else
+            {
+                movieInfo.MpaaRating = "NR";
+            }
+            if (movie.ReleaseDate != null)
+            {
+                movieInfo.ReleaseDate = movie.ReleaseDate.Value.Date;
+            }
+            if (movie.Overview != null)
+            {
+                movieInfo.Synopsis = movie.Overview;
+            }
+            if (movie.Runtime != null)
+            {
+                movieInfo.Duration = movie.Runtime.Value;
+            }
+            if (movie.PosterPath != null)
+            {
+                movieInfo.PosterUrl = "http://image.tmdb.org/t/p/w396" + movie.PosterPath;
+            }
+            else
+            {
+                movieInfo.PosterUrl =
+                    "http://assets.tmdb.org/assets/7f29bd8b3370c71dd379b0e8b570887c/images/no-poster-w185-v2.png";
+            }
+            if (movie.Videos.Results.Where(v => v.Type == "Trailer").FirstOrDefault() != null)
+            {
+                movieInfo.YouTubeTrailer = "http://www.youtube.com/embed/" +
+                                           movie.Videos.Results.Where(v => v.Type == "Trailer").FirstOrDefault().Key;
+            }
+            if (movie.Genres.Count != 0)
+            {
+                foreach (var g in movie.Genres)
+                {
+                    var newGenre = new Models.Genre();
+                    newGenre.GenreName = g.Name;
+                    movieInfo.Genres.Add(newGenre);
+                }
+            }
+            if (movie.Credits.Crew.FirstOrDefault(d => d.Job == "Director") != null)
+            {
+                movieInfo.Director.DirectorName = movie.Credits.Crew.FirstOrDefault(d => d.Job == "Director").Name;
+                movieInfo.Director.DirectorTMDBNum = movie.Credits.Crew.FirstOrDefault(d => d.Job == "Director").Id;
+            }
+            if (movie.ProductionCompanies.Count != 0)
+            {
+                movieInfo.Studio.StudioName = movie.ProductionCompanies[0].Name;
+                movieInfo.Studio.StudioTMDBNum = movie.ProductionCompanies[0].Id;
+            }
+
+            if (movie.AlternativeTitles.Titles.Count != 0)
+            {
+                foreach (var t in movie.AlternativeTitles.Titles)
+                {
+                    if (t.Iso_3166_1 == "US")
+                    {
+                        var newMovieAlias = new MovieAlias();
+                        newMovieAlias.MovieAliasTitle = t.Title;
+                        movieInfo.MovieAliases.Add(newMovieAlias);
+                    }
+                }
+            }
+
+            if (movie.Credits.Cast.Count < 10 && movie.Credits.Cast.Count > 0)
+            {
+                foreach (var a in movie.Credits.Cast)
+                {
+                    Actor newActor = new Actor();
+                    newActor.ActorName = a.Name;
+                    newActor.ActorTMDBNum = a.Id;
+                    newActor.CharacterName = a.Character;
+                    movieInfo.MovieActors.Add(newActor);
+                }
+            }
+            else if (movie.Credits.Cast.Count == 0)
+            {
+                return movieInfo;
+            }
+            else
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Actor newActor = new Actor();
+                    newActor.ActorName = movie.Credits.Cast[i].Name;
+                    newActor.ActorTMDBNum = movie.Credits.Cast[i].Id;
+                    newActor.CharacterName = movie.Credits.Cast[i].Character;
+                    movieInfo.MovieActors.Add(newActor);
+                }
+            }
+
+            return movieInfo;
+        }
+
+
         //Adding a New DVD To the SQL Database (Checks if it already exists as well)
         public DVD AddNewDVDToDBViaTMDB(DVD newDVD)
         {
@@ -1016,115 +1037,6 @@ namespace DVDLibrary.Data
             return newDVD;
         }
 
-
-        //Retrieve TMDB info with a TMDBNum
-        public Models.Movie ReturnMovieInfoFromTMDB(int tmdbNum)
-        {
-            TMDbClient client = new TMDbClient("1fee8f2397ff73412985de2bb825f020");
-
-            Movie movie = client.GetMovie(tmdbNum,
-                MovieMethods.AlternativeTitles | MovieMethods.Credits | MovieMethods.Images | MovieMethods.Releases |
-                MovieMethods.Videos);
-
-            Models.Movie movieInfo = new Models.Movie();
-            movieInfo.MovieTitle = movie.Title;
-            movieInfo.MovieTMDBNum = movie.Id;
-            if (movie.Releases.Countries.FirstOrDefault(m => m.Iso_3166_1 == "US") != null)
-            {
-                movieInfo.MpaaRating = movie.Releases.Countries.FirstOrDefault(m => m.Iso_3166_1 == "US").Certification;
-            }
-            else
-            {
-                movieInfo.MpaaRating = "NR";
-            }
-            if (movie.ReleaseDate != null)
-            {
-                movieInfo.ReleaseDate = movie.ReleaseDate.Value.Date;
-            }
-            if (movie.Overview != null)
-            {
-                movieInfo.Synopsis = movie.Overview;
-            }
-            if (movie.Runtime != null)
-            {
-                movieInfo.Duration = movie.Runtime.Value;
-            }
-            if (movie.PosterPath != null)
-            {
-                movieInfo.PosterUrl = "http://image.tmdb.org/t/p/w396" + movie.PosterPath;
-            }
-            else
-            {
-                movieInfo.PosterUrl =
-                    "http://assets.tmdb.org/assets/7f29bd8b3370c71dd379b0e8b570887c/images/no-poster-w185-v2.png";
-            }
-            if (movie.Videos.Results.Where(v => v.Type == "Trailer").FirstOrDefault() != null)
-            {
-                movieInfo.YouTubeTrailer = "http://www.youtube.com/embed/" +
-                                           movie.Videos.Results.Where(v => v.Type == "Trailer").FirstOrDefault().Key;
-            }
-            if (movie.Genres.Count != 0)
-            {
-                foreach (var g in movie.Genres)
-                {
-                    var newGenre = new Models.Genre();
-                    newGenre.GenreName = g.Name;
-                    movieInfo.Genres.Add(newGenre);
-                }
-            }
-            if (movie.Credits.Crew.FirstOrDefault(d => d.Job == "Director") != null)
-            {
-                movieInfo.Director.DirectorName = movie.Credits.Crew.FirstOrDefault(d => d.Job == "Director").Name;
-                movieInfo.Director.DirectorTMDBNum = movie.Credits.Crew.FirstOrDefault(d => d.Job == "Director").Id;
-            }
-            if (movie.ProductionCompanies.Count != 0)
-            {
-                movieInfo.Studio.StudioName = movie.ProductionCompanies[0].Name;
-                movieInfo.Studio.StudioTMDBNum = movie.ProductionCompanies[0].Id;
-            }
-
-            if (movie.AlternativeTitles.Titles.Count != 0)
-            {
-                foreach (var t in movie.AlternativeTitles.Titles)
-                {
-                    if (t.Iso_3166_1 == "US")
-                    {
-                        var newMovieAlias = new MovieAlias();
-                        newMovieAlias.MovieAliasTitle = t.Title;
-                        movieInfo.MovieAliases.Add(newMovieAlias);
-                    }
-                }
-            }
-
-            if (movie.Credits.Cast.Count < 10 && movie.Credits.Cast.Count > 0)
-            {
-                foreach (var a in movie.Credits.Cast)
-                {
-                    Actor newActor = new Actor();
-                    newActor.ActorName = a.Name;
-                    newActor.ActorTMDBNum = a.Id;
-                    newActor.CharacterName = a.Character;
-                    movieInfo.MovieActors.Add(newActor);
-                }
-            }
-            else if (movie.Credits.Cast.Count == 0)
-            {
-                return movieInfo;
-            }
-            else
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    Actor newActor = new Actor();
-                    newActor.ActorName = movie.Credits.Cast[i].Name;
-                    newActor.ActorTMDBNum = movie.Credits.Cast[i].Id;
-                    newActor.CharacterName = movie.Credits.Cast[i].Character;
-                    movieInfo.MovieActors.Add(newActor);
-                }
-            }
-
-            return movieInfo;
-        }
 
         //Add a New Borrower to DB
         public Borrower AddNewBorrowerToDB(Borrower newBorrower)
--- DVDLibrary.Tests/RepositoryTests.cs
+++ DVDLibrary.Tests/RepositoryTests.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DVDLibrary.Data;
+using DVDLibrary.Models;
 using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
 
@@ -58,6 +59,8 @@ namespace DVDLibrary.Tests
             //looks like I don't need it
         }
 
+
+        //Test RetrieveMoviesListFromDB()
         [Test]
         public void TestRetrieveMoviesListFromDB()
         {
@@ -66,5 +69,73 @@ namespace DVDLibrary.Tests
             Assert.AreEqual(27, moviesCount);
 
         }
+
+
+        //Test RetrieveFullDVDInfoFromDB()
+        [TestCase(1, "Star Wars: Episode IV - A New Hope")]
+        [TestCase(2, "Star Wars: Episode V - The Empire Strikes Back")]
+        [TestCase(26, "Taken")]
+        public void TestRetrieveFullDVDInfoFromDB(int movieId, string expected)
+        {
+            var result = repo.RetrieveFullDVDInfoFromDB(movieId)[0].Movie.MovieTitle;
+
+            Assert.AreEqual(result, expected);
+        }
+
+
+        //Test RetrievePartialDVDsInfo()
+        [TestCase(1, "Star Wars: Episode IV - A New Hope")]
+        [TestCase(2, "Star Wars: Episode V - The Empire Strikes Back")]
+        [TestCase(26, "Taken")]
+        public void TestRetrievePartialDVDsInfo(int movieId, string expected)
+        {
+            var result = repo.RetrievePartialDVDsInfo(movieId)[0].Movie.MovieTitle;
+
+            Assert.AreEqual(result, expected);
+        }
+
+
+        //Test RetrieveMovieIdFromDVDId
+        [TestCase(1, 1)]
+        [TestCase(25, 6)]
+        [TestCase(50, 16)]
+        public void TestRetrieveMovieIdFromDVDId(int dvdId, int expected)
+        {
+            var result = repo.RetrieveMovieIdFromDVDId(dvdId);
+
+            Assert.AreEqual(result, expected);
+        }
+
+
+        //Test ReturnMovieInfoFromTMDB()
+        [TestCase(24428, "The Avengers")]
+        [TestCase(12, "Finding Nemo")]
+        public void TestReturnMovieInfoFromTMDB(int tmdbNum, string expected)
+        {
+            var result = repo.ReturnMovieInfoFromTMDB(tmdbNum).MovieTitle;
+
+            Assert.AreEqual(result, expected);
+        }
+
+
+        //Test AddNewDVDToDBViaTMDB()
+        [Test]
+        public void TestAddNewDVDToDBViaTMDB()
+        {
+            //Adding The Avengers (2012) - TMDB# 24428
+            var newDVD = new DVD();
+            newDVD.Movie = repo.ReturnMovieInfoFromTMDB(24428);
+            newDVD.DVDType = "Blu-Ray";
+
+            var newDVDId = repo.AddNewDVDToDBViaTMDB(newDVD).DVDId;
+
+            Assert.AreEqual(newDVDId, 75);
+        }
+
+
+        //Test AddNewBorrowerToDB()
+
+
+
     }
 }
